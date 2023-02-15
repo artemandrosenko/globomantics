abe2084cd70b3bff5ce4dda1a4b7dd3a95f889f5
Artem Androsenko <tyoma.and@gmail.com>
2023-02-15 21:01:30 +0200

    Add Props for Banner component and test the children prop also

--- components/app.tsx
+++ components/app.tsx
@@ -1,7 +1,16 @@
 import Banner from "./banner";
 
 const App = () => {
-    return <Banner />
+    return <Banner headerText="Providing houses all over the world"/>
 };
+
+// // The Children Prop
+// const App = () => {
+//     return (
+//         <div>
+//             <Banner>Providing houses all over the world</Banner>
+//         </div>
+//     )
+// };
 
 export default App;
--- components/banner.tsx
+++ components/banner.tsx
@@ -7,17 +7,30 @@ const subtitleStyle = {
     color: 'coral'
 }
 
-const Banner = () => {
+const Banner = ({headerText}) => {
     return (
         <header className="row mb-4">
             <div className="col-5">
                 <img src="./GloboLogo.png" alt="logo" className={ logo } />
             </div>
             <div className="col-7 mt-5" style={subtitleStyle}>
-                Providing houses all over the world
+                {headerText}
             </div>
         </header>
     );
 };
+
+// const Banner = ({children}) => {
+//     return (
+//         <header className="row mb-4">
+//             <div className="col-5">
+//                 <img src="./GloboLogo.png" alt="logo" className={ logo } />
+//             </div>
+//             <div className="col-7 mt-5" style={subtitleStyle}>
+//                 {children}
+//             </div>
+//         </header>
+//     );
+// };
 
 export default Banner;
